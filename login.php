3e04878434814ab9e5582c028b179546c318f394
adamecs1 <adam.mihok@seznam.cz>
2025-05-19 20:28:09 +0200

    added logout and head3
    head3 is for a message that is being shown when an user submits wrong username or password, also fixed that the login actually works and not that u can login only with ur username and the password doesnt matter, also made changes to the database, made the heslo column bigger, from varchar(40) ->varchar(100), for the hashed password, didnt know it was actually 60chars long...

--- login.php
+++ login.php
@@ -4,11 +4,14 @@ require "./utils/init.php";
 require "./db/users.php";
 
 ##funkce pro login
+$user = $_SESSION["user"] ?? null;
 
 if(isset($_POST["loginButton"])){
     loginUser($db,$_POST["username"],$_POST["password"]);
-    require "orders.php";
+    require "./orders.php";
 }
+
+
 else{
 require "./layout/head.phtml";
 require "./login.phtml";
--- orders.php
+++ orders.php
@@ -2,6 +2,13 @@
 
 $user = $_SESSION["user"] ?? null; // Pokud není přihlášen, bude null
 
-require "./layout/head2.phtml";
-require "./orders.phtml";
-require "./layout/tail.phtml";
+    if($user===NULL){
+        unset($_SESSION["user"]);
+        require "./layout/head3.phtml";
+        require "./homepage.phtml";
+        require "./layout/tail.phtml";
+    }
+    require "./layout/head2.phtml";
+    require "./orders.phtml";
+    require "./layout/tail.phtml";
+
